9dcc150c4cebafeb823c2d9c5881c2970678fa4d
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-12-08 02:02:00 +0200

    course adding

--- backend/src/controllers/CourseController.ts
+++ backend/src/controllers/CourseController.ts
@@ -93,28 +93,23 @@ export const courseDelete = async (req: Request, res: Response) => {
 
 export const CourseFileAdding = async (req: Request, res: Response) => {
   try {
-    const { userId, courseTitle, category, courseDescription, contentType } =
-      req.body;
-    const user = await User.findOne({ where: { id: userId } });
-    if (user?.role === "admin") {
-      if (!req.file) {
-        res.status(400).json({ message: "No file uploaded" });
-      }
-      await Course.create({
-        title: courseTitle,
-        description: courseDescription,
-        content_type: contentType,
-        category,
-        created_by: userId,
-        file: req.file?.filename,
-      });
-      res.status(200).json({
-        message: "course uploaded successfully",
-        file: req.file,
-      });
-    } else {
-      res.json({ message: "you are not allowed adding courses" });
+    const { courseTitle, category, courseDescription, contentType } = req.body;
+
+    if (!req.file) {
+      res.status(400).json({ message: "No file uploaded" });
     }
+    await Course.create({
+      title: courseTitle,
+      description: courseDescription,
+      content_type: contentType,
+      category,
+      created_by: userId,
+      file: req.file?.filename,
+    });
+    res.status(200).json({
+      message: "course uploaded successfully",
+      file: req.file,
+    });
   } catch (error) {
     res.json({ message: error });
   }
